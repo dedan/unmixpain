f84cffc64670a8326f0517bbc18f999a6cdea5a2
Stephan Gabler <stephan.gabler@gmail.com>
2011-11-23 16:10:27 +0100

    add cv vs. force plot

--- labchart2neo.py
+++ labchart2neo.py
@@ -5,7 +5,8 @@ from nexioplus import NexIOplus
 import pylab as plt
 import numpy as np
 import quantities as pq
-import math
+from math import floor
+import os
 
 # logger setup
 logging.basicConfig(level=logging.DEBUG,
@@ -14,38 +15,108 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger()
 
 data_folder = '/Users/dedan/projects/fu/data/'
-downsample_factor = 100
+downsample_factor = 10
+boring_thresh = 1
+stepper_thresh = 0.1
+win_width = 100
+
+
+def extract_onsets(signal, threshold, win_width):
+    """find onsets in stepper signal"""
+    s = np.convolve(abs(signal), np.ones(win_width) / win_width)
+    x = 0
+    onoffs = []
+    for i, y in enumerate(s):
+        if y > threshold:
+            if x == 0:
+                x = i
+        elif not x == 0:
+            onoffs.append((x, i))
+            x = 0
+    # sort them differently because my algo is so stupid
+    onoffs = [(onoffs[i][1], onoffs[i+1][0]) for i in range(len(onoffs)-1)]
+    # don't use the times in between
+    return onoffs[::2]
+
+def is_boring_segment(signals, threshold):
+    """docstring for is_boring_segment"""
+    no_temp_change = np.max(signals[2]) - np.min(signals[2]) < threshold
+    no_stepper = np.max(signals[0]) - np.min(signals[0]) < threshold
+    return no_temp_change and no_stepper
+
+def cv(train):
+    """compute the coefficient of variation"""
+    isi = np.diff(win)
+    return np.std(isi)/np.mean(isi)
+
+
 
 nexlist = glob.glob(data_folder + '*.nex')
 
+res = {}
+
 # actual data processing
+# for nexname in [nexlist[2]]:
 for i, nexname in enumerate(nexlist):
 
+    print " "
     logger.info('read in: %s' % nexname)
     block = NexIOplus(filename=nexname, downsample=downsample_factor).read()
 
+    res[nexname] = {"flevels": [], "cvs": []}
+
     plt.figure()
-    for j, segments in enumerate(block.segments):
+    for segment in block.segments:
 
-        plt.subplot(3, 3, j+1)
-        rate = segments.analogsignals[0].sampling_rate
-        length = len(segments.analogsignals[0])
-        start = segments.analogsignals[0].t_start * pq.s
+        if is_boring_segment(segment.analogsignals, boring_thresh):
+            continue
 
-        for signal in segments.analogsignals:
-            plt.plot(signal)
+        stepper = segment.analogsignals[0]
+        force = segment.analogsignals[1]
+        temp = segment.analogsignals[2]
 
-        train = segments.spiketrains[0]
+        rate = stepper.sampling_rate * 1/pq.s
+        length = len(stepper)
+        start = stepper.t_start * pq.s
+
+        x_range = range(int(floor(start*rate)), int(floor(start*rate))+length)
+        # plt.plot(x_range, stepper, 'b')
+        plt.plot(x_range, force, 'g')
+        plt.plot(x_range, temp, 'r')
+
+        onoffs = extract_onsets(stepper, stepper_thresh, win_width)
+        for x1, x2 in onoffs:
+            flevel = np.max(force[x1:x2]) - np.min(force)
+            res[nexname]["flevels"].append(flevel)
+            l = plt.plot([x1 + start*rate, x2 + start*rate], [0, 0], 'v')
+            l[0].set_markersize(10)
+
+        train = segment.spiketrains[0]
         if np.any(train):
             tmp = np.zeros(length)
-            isi = np.diff(train)
-            print "CV: %f" % (np.std(isi)/np.mean(isi))
-
             for spike in train:
-                tmp[int(math.floor((spike-start)*rate))] = 1
+                tmp[int(floor((spike - start) * rate))] = 1
+            plt.plot(x_range, tmp, 'k', linewidth=0.3)
 
-            plt.plot(tmp)
+        onoffs_time = [(x1/rate, x2/rate) for x1, x2 in onoffs]
+        if onoffs_time:
+            for x1, x2 in onoffs_time:
+                win = train[(train > x1+start) & (train < x2+start)]
+                res[nexname]["cvs"].append(cv(win))
+                print "CV: %f\n" % cv(win)
+    bla = plt.xticks()
+    plt.xticks(bla[0], bla[0]/rate, rotation=25)
 
 
-    plt.savefig("fig%d.pdf" % (i+1))
+    plt.show()
+    # plt.savefig("fig%d.pdf" % (i+1))
+
+plt.figure()
+plt.axes([0.1, 0.1, 0.5, 0.8])
+plt.title("CV against force level")
+for key, bla in res.items():
+    plt.plot(bla['flevels'], bla['cvs'], '*', label=os.path.basename(key))
+plt.legend(loc=(1, 0))
+# plt.savefig("cv_vs_force.pdf")
+plt.show()
 
